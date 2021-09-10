0a3c98ae33571ac847fd6d78b2eae117890ad37d
Michael Cadigan <michael@numo.com>
2021-09-10 10:31:12 -0400

    Update instagram_private_api.dart

--- lib/instagram_private_api.dart
+++ lib/instagram_private_api.dart
@@ -4,5 +4,8 @@
 library instagram_private_api;
 
 export 'package:instagram_private_api/src/core/insta_client.dart';
+export 'package:instagram_private_api/src/core/insta_state.dart';
+export 'package:instagram_private_api/src/utilities/insta_cookie_jar.dart';
 
 // TODO: Export any libraries intended for clients of this package.
+gi
